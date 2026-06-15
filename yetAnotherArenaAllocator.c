ca7a4ec1b85ab9509361ef793910da85ad7fd098
James Thai <jamesthai79@gmail.com>
2026-06-15 05:29:29 -0700

    createArena, arenaPush, and freeArena functions created

--- yetAnotherArenaAllocator.c
+++ yetAnotherArenaAllocator.c
@@ -1,8 +1,35 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  uint64_t pos;
+  uint64_t capacity;
+} arena;
+
+arena *createArena(uint64_t capacity) {
+  arena *memarena = (arena *)malloc(sizeof(arena) + capacity);
+  memarena->capacity = capacity;
+  memarena->pos = 0;
+  return memarena;
+}
+
+void *arenaPush(arena *memarena, uint64_t size) {
+  if (memarena->pos + size > memarena->capacity) {
+    return NULL;
+  }
+  uint8_t *out = (uint8_t *)memarena + size;
+  memarena->pos = memarena->pos + size;
+  return out;
+}
+void freeArena(arena *memarena) { free(memarena); }
 
 int main(void) {
-
-  printf("I'm attempting to write an arena allocator but literally only "
-         "watching videos ;-;");
+  arena *testArena = createArena(1024);
+  int *test = (int *)arenaPush(testArena, sizeof(int));
+  *test = 10;
+  printf("test: %d", *test);
+  free(testArena);
   return 0;
 }
